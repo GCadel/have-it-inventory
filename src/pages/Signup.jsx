e279ddc38736936242d255f10f16e6a6b58093c5
GCMoony <47188474+GCMoony@users.noreply.github.com>
2025-10-16 21:15:44 +0900

    Allow signup data to be submitted

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,32 +1,47 @@
-import { useEffect, useRef, useState } from 'react';
+import { useRef, useState } from 'react';
 import { Button } from '../shared/Button/Button';
 
 import { Link, useNavigate } from 'react-router';
 import { ButtonContainer } from '../shared/ButtonContainer';
 import FormField from '../shared/FormField';
+import { UserAuth } from '../context/AuthContext';
 
 export const Signup = () => {
   const navigate = useNavigate();
   const signupForm = useRef(null);
-  const [success, setSuccess] = useState(false);
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
+  const { signupNewUser } = UserAuth();
 
-  function handleSubmit(e) {
+  async function handleSignup(e) {
     e.preventDefault();
+    setLoading(true);
     const formData = new FormData(e.target);
-    alert(`Thanks for signing up, ${formData.get('name')}`);
+    try {
+      const result = await signupNewUser(
+        formData.get('email'),
+        formData.get('password'),
+        formData.get('name')
+      );
+
+      if (error) {
+        console.error('Signup unsuccessful', result);
+      }
+      navigate('/dashboard');
+    } catch (error) {
+      setError('An error has occurred');
+      console.error('Signup error:', error);
+    } finally {
+      setLoading(false);
+    }
   }
 
   return (
     <>
       <h2>Get Started With Have-It</h2>
       <p>Create an account now!</p>
-      <form id="signup-form" ref={signupForm} onSubmit={handleSubmit}>
-        <div>
-          {error ? <p>Something went wrong: {`${error}`}</p> : ``}
-          {success ? <p>Please check your email for your magic link</p> : ''}
-        </div>
+      <form id="signup-form" ref={signupForm} onSubmit={handleSignup}>
+        <div>{error ? <p>Something went wrong: {`${error}`}</p> : ``}</div>
         <FormField
           name="name"
           type={'text'}
