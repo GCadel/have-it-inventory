cee85562db1285222359f7fdd819f3f0f2f8312d
GCMoony <47188474+GCMoony@users.noreply.github.com>
2025-10-21 03:02:30 +0900

    Add ability to update part count and delete part from assembly

--- src/api/assembly_parts.js
+++ src/api/assembly_parts.js
@@ -2,15 +2,19 @@ import { supabase } from '../lib/supabase';
 
 const TABLE = 'assembly_parts';
 
-export async function createAssemblyPart(assemblyId, partId, quantity) {
+export async function createAssemblyPart(
+  assemblyId,
+  partId,
+  partName,
+  quantity
+) {
   const part = {
     assembly_id: assemblyId,
     part_id: partId,
-    quantity: 4,
+    quantity: quantity,
+    part_name: partName,
   };
-
   const { error, data } = await supabase.from(TABLE).insert(part).select();
-  console.log(data);
   return { error, data };
 }
 
@@ -18,8 +22,22 @@ export async function getPartsByAssemblyId(assemblyId) {
   const { error, data } = await supabase
     .from(TABLE)
     .select()
-    .eq('assembly_id', assemblyId);
-  console.log('getting parts by assembly id');
-  console.log(error, data);
+    .eq('assembly_id', assemblyId)
+    .order('created_at', { ascending: true });
   return { error, data };
+}
+
+export async function updateAssemblyPartQuantity(assemblyPartId, quantity) {
+  const { error, data } = await supabase
+    .from(TABLE)
+    .update({ quantity: quantity })
+    .eq('id', assemblyPartId);
+
+  return { error, data };
+}
+
+export async function deleteAssemblyPartById(assemblyPartId) {
+  const res = await supabase.from(TABLE).delete().eq('id', assemblyPartId);
+
+  return res;
 }
