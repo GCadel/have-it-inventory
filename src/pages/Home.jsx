a8e4d4e77d74e8c74699d8b1142d580e06fd5e23
GCMoony <47188474+GCMoony@users.noreply.github.com>
2025-10-21 10:33:18 +0900

    Added sample landing page images

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,28 +2,36 @@ import { useNavigate } from 'react-router';
 import { Button } from '../shared/Button/Button';
 import { ButtonContainer } from '../shared/ButtonContainer';
 import { CenteredText } from '../shared/CenteredText';
+import tableDemo from '../assets/demo-img-01.png';
+import assemblyDemo from '../assets/demo-img-02.png';
 
 export const Home = () => {
   const navigate = useNavigate();
   return (
-    <>
-      <CenteredText>
-        <h2>Have-It</h2>
-        <p>Manage your inventory anywhere</p>
-      </CenteredText>
-      <br />
-      <ButtonContainer>
-        <Button
-          text={'Login'}
-          action={() => navigate('/login')}
-          buttonType={'secondary'}
-        />
-        <Button
-          text={'Get Started'}
-          action={() => navigate('/signup')}
-          buttonType={'primary'}
-        />
-      </ButtonContainer>
-    </>
+    <div className="flex-between">
+      <div className="demo-image-holder">
+        <img src={tableDemo} alt="Table Demo" />
+        <img src={assemblyDemo} alt="Assembly Demo" />
+      </div>
+      <div>
+        <CenteredText>
+          <h2>Have-It</h2>
+          <p>Manage your inventory anywhere</p>
+        </CenteredText>
+        <br />
+        <ButtonContainer>
+          <Button
+            text={'Login'}
+            action={() => navigate('/login')}
+            buttonType={'secondary'}
+          />
+          <Button
+            text={'Get Started'}
+            action={() => navigate('/signup')}
+            buttonType={'primary'}
+          />
+        </ButtonContainer>
+      </div>
+    </div>
   );
 };
